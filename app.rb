d22224e7f48702fc0659d42df283b4f3ba3bd210
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-21 09:20:11 -0400

    fixed issues by taking out puts

--- app.rb
+++ app.rb
@@ -13,21 +13,16 @@ post '/contact' do
 	contact = params[:contact]
 	address = params[:address]
 	phone = params[:phone]
+	# puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 20"
 	redirect '/confirm?contact=' + contact + '&address=' + address + '&phone=' + phone
-		puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 20"
+		
 	end
 
 get '/confirm' do
 	contact = params[:contact]
 	address = params[:address]
 	phone = params[:phone]
+	# puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 28"
 	erb :confirm, :locals => {:contact => contact, :address => address, :phone => phone}
-	puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 28"
-end
-
-post 'confirm' do
-	contact = params[:contact]
-	address = params[:address]
-	phone = params[:phone]
-	puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 32"
+	
 end
