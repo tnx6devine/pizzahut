9041c361d320ed74ee865fa4d07d6eb1fd4893c1
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-21 08:36:03 -0400

    ends in blank page, home page working

--- app.rb
+++ app.rb
@@ -2,16 +2,32 @@ require "sinatra"
 
 
 get '/'  do
-	redirect '/names'
+	redirect '/orders'
 end
 
-get '/names' do
-	erb :names 
+get '/orders' do
+	erb :orders
 end
 
-post '/list' do
-	names_provided = params[:names_provided]
-	names_provided.each do |name|
-		puts name
+post '/contact' do
+	contact = params[:contact]
+	address = params[:address]
+	phone = params[:phone]
+	redirect '/confirm?contact=' + contact + '&address=' + address + '&phone=' + phone
+		puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 20"
 	end
+
+get '/confirm' do
+	contact = params[:contact]
+	address = params[:address]
+	phone = params[:phone]
+	erb :confirm, :locals => {:contact => contact, :address => address, :phone => phone}
+	puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 28"
+end
+
+post 'confirm' do
+	contact = params[:contact]
+	address = params[:address]
+	phone = params[:phone]
+	puts "We will contact #{contact} at #{phone} from our location nearest #{address}. LINE 32"
 end
---
+++ public/scripts.js
@@ -0,0 +1,3 @@
+function menu() {
+		alert('Hello!  Welcome to the Pizza Hut Menu.\nWe have the following menu items to choose from:\n\nPizza\nCheese Sticks\nBread Sticks\n2 Liter Pepsi Products');
+	}
