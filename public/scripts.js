1ba0628cf2d94c8a9971c6bd279c86e9adb6bfac
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-24 10:22:21 -0400

    fixed order for pizza

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,4 @@
 function menu() {
 		alert('Hello!  Welcome to the Pizza Hut Menu.\nWe have the following menu items to choose from:\n\nPizza\nCheese Sticks\nBread Sticks\n2 Liter Pepsi Products');
-}
-
-
-function addMenuItem() {
-	alert("enter function");
-	var menuitem = document.getElementById("menuitem").value;
-	if (menuitem == "pizza") {
-		var input = '<input type="text" name="toppings" placeholder="enter toppings">';
-		document.getElementById("toppings").innerHTML = input;;
-	}
 
 }
